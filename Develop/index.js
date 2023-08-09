1339cc82b7ad76244f10a48217aca503a946e5ee
YJRose <yj.lin963320@gmail.com>
2023-08-08 22:45:44 -0400

    added question prompt code in inde.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,33 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["motivation", "reasons", "problem-solving", "learning"];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What was your motivation?',
+            name: 'motivation',  
+        },
+        {
+            type: 'input',
+            message: 'Why did you build this project?',
+            name: 'reasons',
+        },
+        {
+            type: 'input',
+            message: 'What problem does it solve?',
+            name: 'problem-solving',
+        },
+        {
+            type: 'input',
+            message: 'What did you learn?',
+            name: 'learning',
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
