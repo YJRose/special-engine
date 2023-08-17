ae8975b36e9473b1637d53433d749210e90d10d1
YJRose <yj.lin963320@gmail.com>
2023-08-16 21:06:21 -0400

    refine both generateMarkdown and index files

---
+++ Develop/README_.md
@@ -0,0 +1,20 @@
+
+  
+  # 123
+
+  ## Description
+
+  343
+
+  ## Table of Contents
+
+  2435(https://github.com/YJRose/special-engine/blob/d010caedd91d43179b98dc832a49bb65408c5d0b/README.md)
+
+  ### License
+
+  ![Github License](https://img.shields.io/badge/license-MIT-blue.svg);
+
+  ### YJRose(https://github.com/YJRose)
+
+  ---
+  please reach out to me if you have further question via yj.lin963320@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return `![Github License] (https://img.shields.io/badge/license-${license}-blue.svg)`
+  return `![Github License](https://img.shields.io/badge/license-${license}-blue.svg)`
 }
 
 // TODO: Create a function that returns the license link
@@ -16,13 +16,24 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `
   
-  ##${data.title}
-
-  ${renderLicenseBadge(data.license)};
+  # ${data.title}
 
   ## Description
 
   ${data.description}
+
+  ## Table of Contents
+
+  ${data.contents}(https://github.com/YJRose/special-engine/blob/d010caedd91d43179b98dc832a49bb65408c5d0b/README.md)
+
+  ### License
+
+  ${renderLicenseBadge(data.license)};
+
+  ### ${data.githubusername}(https://github.com/${data.githubusername})
+
+  ---
+  please reach out to me if you have further question via ${data.email}
 `;
 }
 
